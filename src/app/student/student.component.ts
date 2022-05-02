0358d945fba5838bd3cb117b002ab72928a903b0
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-05-02 12:23:37 +0530

    initial file and some component creation

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-student',
+    templateUrl:'./student.component.html',
+    styleUrls:['./student.component.css'],
+})
+
+export class StudentComponent
+{
+    name:string = "sohel";
+    age:number = 23;
+    location : string = "solapur"
+}
